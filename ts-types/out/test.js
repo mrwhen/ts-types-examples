25da47440c29e696007c3f6fe932b2b4a571e80f
ext.ligen7 <ext.ligen7@jd.com>
2023-08-07 19:36:15 +0800

    feat: done

---
+++ ts-types/out/test.js
@@ -0,0 +1,13 @@
+"use strict";
+// let hp: number = 100; // 基础血量
+// let skill_name: string = '烈焰斩'; // 技能名字
+// let is_cd: boolean = false; // 技能冷却状态
+// console.log(typeof skill_name)
+// skill_name = '烈焰斩';
+// console.log(typeof skill_name)
+// skill_name = 11
+let hp = 100;
+console.log(typeof hp);
+hp = "study";
+console.log(typeof hp);
+//# sourceMappingURL=test.js.map
